2df2b82175bf273d7cabf7b02de7ceee79ee9399
tnkbang <tnkbang@cusc.ctu.edu.vn>
2024-04-01 11:24:21 +0900

    Change pass and port default of Lavalink

--- Program.cs
+++ Program.cs
@@ -19,6 +19,14 @@ builder.Services.AddHostedService<BotService>();
 
 // Lavalink
 builder.Services.AddLavalink();
+builder.Services.ConfigureLavalink(config =>
+{
+	config.BaseAddress = new Uri("http://localhost:2323");
+	config.WebSocketUri = new Uri("ws://localhost:2323/v4/websocket");
+	//config.ReadyTimeout = TimeSpan.FromSeconds(10);
+	//config.ResumptionOptions = new LavalinkSessionResumptionOptions(TimeSpan.FromSeconds(60));
+	config.Passphrase = "on6tyeqwxdtvo3ld";
+});
 builder.Services.AddLogging(x => x.AddConsole().SetMinimumLevel(LogLevel.Trace));
 
 await builder.Build().RunAsync();
