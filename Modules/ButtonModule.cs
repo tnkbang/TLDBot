79779426c0d065f1b7b00383b6c53a478ebc653c
tnkbang <tnkbang@cusc.ctu.edu.vn>
2024-05-21 11:20:46 +0900

    Not set null for properties of ButtonModule

--- Modules/ButtonModule.cs
+++ Modules/ButtonModule.cs
@@ -9,8 +9,8 @@ namespace TLDBot.Modules
 {
 	public class ButtonModule
 	{
-		private readonly ButtonMusicHandler? _musicHandler = null;
-		private readonly HooHeyHowHandler? _heyHowHandler = null;
+		private readonly ButtonMusicHandler _musicHandler;
+		private readonly HooHeyHowHandler _heyHowHandler;
 
 		public ButtonModule(IAudioService audioService, SocketMessageComponent messageComponent, SocketCommandContext context)
 		{
@@ -38,41 +38,41 @@ namespace TLDBot.Modules
 		}
 
 		#region Music
-		public async Task DisconnectAsync() => await _musicHandler!.DisconnectAsync().ConfigureAwait(false);
+		public async Task DisconnectAsync() => await _musicHandler.DisconnectAsync().ConfigureAwait(false);
 
-		public async Task PositionAsync() => await _musicHandler!.PositionAsync().ConfigureAwait(false);
+		public async Task PositionAsync() => await _musicHandler.PositionAsync().ConfigureAwait(false);
 
-		public async Task StopAsync() => await _musicHandler!.StopAsync().ConfigureAwait(false);
+		public async Task StopAsync() => await _musicHandler.StopAsync().ConfigureAwait(false);
 
-		public async Task SkipAsync() => await _musicHandler!.SkipAsync().ConfigureAwait(false);
+		public async Task SkipAsync() => await _musicHandler.SkipAsync().ConfigureAwait(false);
 
-		public async Task LoopAsync() => await _musicHandler!.LoopAsync().ConfigureAwait(false);
+		public async Task LoopAsync() => await _musicHandler.LoopAsync().ConfigureAwait(false);
 
-		public async Task ShuffleAsync() => await _musicHandler!.ShuffleAsync().ConfigureAwait(false);
+		public async Task ShuffleAsync() => await _musicHandler.ShuffleAsync().ConfigureAwait(false);
 
-		public async Task SeekPrev10SAsync() => await _musicHandler!.SeekAsync(new TimeSpan(00, 00, -10)).ConfigureAwait(false);
+		public async Task SeekPrev10SAsync() => await _musicHandler.SeekAsync(new TimeSpan(00, 00, -10)).ConfigureAwait(false);
 
-		public async Task SeekNext10SAsync() => await _musicHandler!.SeekAsync(new TimeSpan(00, 00, 10)).ConfigureAwait(false);
+		public async Task SeekNext10SAsync() => await _musicHandler.SeekAsync(new TimeSpan(00, 00, 10)).ConfigureAwait(false);
 
-		public async Task PauseAsync() => await _musicHandler!.PauseAsync().ConfigureAwait(false);
+		public async Task PauseAsync() => await _musicHandler.PauseAsync().ConfigureAwait(false);
 
-		public async Task ResumeAsync() => await _musicHandler!.ResumeAsync().ConfigureAwait(false);
+		public async Task ResumeAsync() => await _musicHandler.ResumeAsync().ConfigureAwait(false);
 
-		public async Task QueueAsync() => await _musicHandler!.QueueAsync().ConfigureAwait(false);
+		public async Task QueueAsync() => await _musicHandler.QueueAsync().ConfigureAwait(false);
 		#endregion
 
 		#region Game HooHeyHow
-		public async Task DeerAsync() => await _heyHowHandler!.RespondAsync(HooHeyHowHandler.DEER).ConfigureAwait(false);
+		public async Task DeerAsync() => await _heyHowHandler.RespondAsync(HooHeyHowHandler.DEER).ConfigureAwait(false);
 
-		public async Task CalabashAsync() => await _heyHowHandler!.RespondAsync(HooHeyHowHandler.CALABASH).ConfigureAwait(false);
+		public async Task CalabashAsync() => await _heyHowHandler.RespondAsync(HooHeyHowHandler.CALABASH).ConfigureAwait(false);
 
-		public async Task ChickenAsync() => await _heyHowHandler!.RespondAsync(HooHeyHowHandler.CHICKEN).ConfigureAwait(false);
+		public async Task ChickenAsync() => await _heyHowHandler.RespondAsync(HooHeyHowHandler.CHICKEN).ConfigureAwait(false);
 
-		public async Task FishAsync() => await _heyHowHandler!.RespondAsync(HooHeyHowHandler.FISH).ConfigureAwait(false);
+		public async Task FishAsync() => await _heyHowHandler.RespondAsync(HooHeyHowHandler.FISH).ConfigureAwait(false);
 
-		public async Task CrabAsync() => await _heyHowHandler!.RespondAsync(HooHeyHowHandler.CRAB).ConfigureAwait(false);
+		public async Task CrabAsync() => await _heyHowHandler.RespondAsync(HooHeyHowHandler.CRAB).ConfigureAwait(false);
 
-		public async Task LobsterAsync() => await _heyHowHandler!.RespondAsync(HooHeyHowHandler.LOBSTER).ConfigureAwait(false);
+		public async Task LobsterAsync() => await _heyHowHandler.RespondAsync(HooHeyHowHandler.LOBSTER).ConfigureAwait(false);
 		#endregion
 	}
 }
