78e1926b441c12692c76343ef597b2140a40f150
tnkbang <tnkbang@cusc.ctu.edu.vn>
2024-07-10 12:31:26 +0900

    Update lavalink to version 4.0.7 (using youtube-plugin)

--- Services/Lavalink.cs
+++ Services/Lavalink.cs
@@ -7,9 +7,14 @@ namespace TLDBot.Services
 		private static Process _process = new Process();
 		public static void Start()
 		{
-			ProcessStartInfo psi = new ProcessStartInfo("java.exe", " -jar " + Environment.CurrentDirectory + "\\Lavalink.jar");
-			psi.CreateNoWindow = true;
-			psi.UseShellExecute = true;
+			ProcessStartInfo psi = new ProcessStartInfo
+			{
+				FileName = "java.exe",
+				Arguments = "-jar " + Environment.CurrentDirectory + "\\Lavalink\\Lavalink.jar",
+				WorkingDirectory = Environment.CurrentDirectory + "\\Lavalink",
+				CreateNoWindow = true,
+				UseShellExecute = true
+			};
 			_process = new Process();
 			_process.StartInfo = psi;
 			_process.Start();
